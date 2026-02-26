44cd8558b21cb6cc7a6a1ba33b6261dc9ae46828
sundararajan-git <sundararajanselvarasu@gmail.com>
2026-02-26 13:34:38 +0530

    dark them support

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { Button } from "./components/ui/button";
 import "./App.css";
+import { ModeToggle } from "./components/shared/ModeToggle";
 
 const App = () => {
   return (
     <div>
       <p>App</p>
       <Button>Click me</Button>
+      <ModeToggle />
     </div>
   );
 };
