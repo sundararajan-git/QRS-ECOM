edf46f760b9de0cdbb98a1d8e2ae76e4cac10194
sundararajan-git <sundararajanselvarasu@gmail.com>
2026-02-27 10:40:55 +0530

    error toast handler

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,27 @@
+import type { ErrorToastType } from "@/types";
 import { clsx, type ClassValue } from "clsx"
+import toast from "react-hot-toast";
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export const showErrorToast = (err: ErrorToastType) => {
+  console.error(err);
+  if (typeof err === "object" && err !== null && "response" in err) {
+    const errorWithResponse = err as {
+      response?: { data?: { message?: string } };
+    };
+    if (errorWithResponse.response?.data?.message) {
+      toast.error(err?.response?.data?.message || "Something went wrong !");
+    } else {
+      toast.error("An unexpected error occurred.");
+    }
+  } else if (err instanceof Error) {
+    toast.error(err.message);
+  } else {
+    toast.error(String(err));
+  }
+};
